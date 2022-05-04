6bd7f43d8016c6d729032df36945253eb14935e9
Sejo Basic <sejoob@gmail.com>
2022-05-03 22:08:58 -0700

    Added Tilt effect to images

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react'
 import '../styling/photo.css'
 import close from '../assets/close.png'
 import designData  from '../utils/designData'
+import Tilt from 'react-parallax-tilt';
 
 
 function Photo() {
@@ -39,7 +40,9 @@ const [tempImgSrc, setTempImgSrc] = useState('')
         <div className='reel-item'>&nbsp;+</div>
       </div>
       <div className={modal ? 'modal open' : 'modal'}>
+        <Tilt glareEnable={true} glareColor='#fbdce2' glarePosition='all' glareMaxOpacity={0.5} perspective={800} gyroscope={true} tiltMaxAngleX={35} tiltMaxAngleY={35}>
         <img className='modal-img' src={tempImgSrc} alt='images modal' onClick={() => setModal(false)}  />
+        </Tilt>
         <img className='close' src={close} alt='close button' onClick={() => setModal(false)} />
         <p>PHOTOGRAPHY AND DESIGN WORK BY SEJO BASIC</p>
       </div>
