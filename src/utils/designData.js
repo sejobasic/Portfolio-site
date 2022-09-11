2d3720baa43ca9c2e2ce01020aa90c5997367a54
Sejo Basic <sejoob@gmail.com>
2022-09-11 14:35:18 -0700

    data fix

--- src/utils/designData.js
+++ src/utils/designData.js
@@ -2,194 +2,194 @@
 export default [
   {
     id: 1,
-    imgSrc: ('https://i.ibb.co/6433tFV/design1.png')
+    imgSrc: require('../design/design1.png')
   },
   {
     id: 2,
-    imgSrc: ('https://i.ibb.co/9GWQNRC/design2.png')
+    imgSrc: require('../design/design2.png')
   },
   {
     id: 3,
-    imgSrc: ('https://i.ibb.co/xztL1qZ/design3.jpg')
+    imgSrc: require('../design/design3.png')
   },
   {
     id: 4,
-    imgSrc: ('https://i.ibb.co/8xsTw1f/design4.png')
+    imgSrc: require('../design/design4.png')
   },
   {
     id: 5,
-    imgSrc: ('https://i.ibb.co/t83hq15/design46.png')
+    imgSrc: require('../design/design45.png')
   },
   {
     id: 6,
-    imgSrc: ('https://i.ibb.co/3s5MNz8/design47.png')
+    imgSrc: require('../design/design46.png')
   },
   {
     id: 7,
-    imgSrc: ('https://i.ibb.co/Pcc6mtw/design44.png')
+    imgSrc: require('../design/design47.png')
   },
   {
     id: 8,
-    imgSrc: ('https://i.ibb.co/H4TMnVd/design8.png')
+    imgSrc: require('../design/design42.png')
   },
   {
     id: 9,
-    imgSrc: ('https://i.ibb.co/vVS1yXX/design9.png')
+    imgSrc: require('../design/design43.png')
   },
   {
     id: 10,
-    imgSrc: ('https://i.ibb.co/xmXXskC/design10.png')
+    imgSrc: require('../design/design44.png')
   },
   {
     id: 11,
-    imgSrc: ('https://i.ibb.co/5jKvKyx/design42.png')
+    imgSrc: require('../design/design48.png')
   },
   {
     id: 12,
-    imgSrc: ('https://i.ibb.co/QMVZHj4/design43.png')
+    imgSrc: require('../design/design12.png')
   },
   {
     id: 13,
-    imgSrc: ('https://i.ibb.co/Lnp2kJg/design13.png')
+    imgSrc: require('../design/design13.png')
   },
   {
     id: 14,
-    imgSrc: ('https://i.ibb.co/qDPjdF7/design45.png')
+    imgSrc: require('../design/design14.png')
   },
   {
     id: 15,
-    imgSrc: ('https://i.ibb.co/S3K8Xmt/design48.png')
+    imgSrc: require('../design/design15.png')
   },
   {
     id: 16,
-    imgSrc: ('https://i.ibb.co/C5bQFRK/design16.jpg')
+    imgSrc: require('../design/design16.png')
   },
   {
     id: 17,
-    imgSrc: ('https://i.ibb.co/tHn0kBF/design17.png')
+    imgSrc: require('../design/design17.png')
   },
   {
     id: 18,
-    imgSrc: ('https://i.ibb.co/Vqx9Ckw/design18.jpg')
+    imgSrc: require('../design/design18.png')
   },
   {
     id: 19,
-    imgSrc: ('https://i.ibb.co/zbysnM9/design19.png')
+    imgSrc: require('../design/design19.png')
   },
   {
     id: 20,
-    imgSrc: ('https://i.ibb.co/PFBG54Z/design20.png')
+    imgSrc: require('../design/design20.png')
   },
   {
     id: 21,
-    imgSrc: ('https://i.ibb.co/hgsjmgM/design21.png')
+    imgSrc: require('../design/design21.png')
   },
   {
     id: 22,
-    imgSrc: ('https://i.ibb.co/GC2LXRt/design22.png')
+    imgSrc: require('../design/design22.png')
   },
   {
     id: 23,
-    imgSrc: ('https://i.ibb.co/D8Z3PTS/design23.png')
+    imgSrc: require('../design/design23.png')
   },
   {
     id: 24,
-    imgSrc: ('https://i.ibb.co/SnXRgXb/design24.png')
+    imgSrc: require('../design/design24.png')
   },
   {
     id: 25,
-    imgSrc: ('https://i.ibb.co/YTshRC6/design25.png')
+    imgSrc: require('../design/design25.png')
   },
   {
     id: 26,
-    imgSrc: ('https://i.ibb.co/HFYKvhv/design26.jpg')
+    imgSrc: require('../design/design26.png')
   },
   {
     id: 27,
-    imgSrc: ('https://i.ibb.co/bv4hW2K/design27.jpg')
+    imgSrc: require('../design/design27.png')
   },
   {
     id: 28,
-    imgSrc: ('https://i.ibb.co/0h0mzSM/design28.png')
+    imgSrc: require('../design/design28.png')
   },
   {
     id: 29,
-    imgSrc: ('https://i.ibb.co/QfV2yds/design29.jpg')
+    imgSrc: require('../design/design29.png')
   },
   {
     id: 30,
-    imgSrc: ('https://i.ibb.co/PgqgDGN/design30.png')
+    imgSrc: require('../design/design30.png')
   },
   {
     id: 31,
-    imgSrc: ('https://i.ibb.co/SQdH36n/design31.png')
+    imgSrc: require('../design/design31.png')
   },
   {
     id: 32,
-    imgSrc: ('https://i.ibb.co/X5sRPvX/design32.png')
+    imgSrc: require('../design/design32.png')
   },
   {
     id: 33,
-    imgSrc: ('https://i.ibb.co/t8sLJwL/design33.png')
+    imgSrc: require('../design/design33.png')
   },
   {
     id: 34,
-    imgSrc: ('https://i.ibb.co/yBkwNrZ/design34.jpg')
+    imgSrc: require('../design/design34.png')
   },
   {
     id: 35,
-    imgSrc: ('https://i.ibb.co/9WRMSjD/design35.png')
+    imgSrc: require('../design/design35.png')
   },
   {
     id: 36,
-    imgSrc: ('https://i.ibb.co/1QHYsYh/design36.png')
+    imgSrc: require('../design/design36.png')
   },
   {
     id: 37,
-    imgSrc: ('https://i.ibb.co/hdPCC4j/design37.png')
+    imgSrc: require('../design/design37.png')
   },
   {
     id: 38,
-    imgSrc: ('https://i.ibb.co/CKrSLjr/design38.png')
+    imgSrc: require('../design/design38.png')
   },
   {
     id: 39,
-    imgSrc: ('https://i.ibb.co/mtgLctz/design39.png')
+    imgSrc: require('../design/design39.png')
   },
   {
     id: 40,
-    imgSrc: ('https://i.ibb.co/gJmjpdr/design40.png')
+    imgSrc: require('../design/design40.png')
   },
   {
     id: 41,
-    imgSrc: ('https://i.ibb.co/LgQzVQB/design41.png')
+    imgSrc: require('../design/design41.png')
   },
   {
     id: 42,
-    imgSrc: ('https://i.ibb.co/Zfrb26H/design5.jpg')
+    imgSrc: require('../design/design5.png')
   },
   {
     id: 43,
-    imgSrc: ('https://i.ibb.co/rv2yRgY/design6.png')
+    imgSrc: require('../design/design6.png')
   },
   {
     id: 44,
-    imgSrc: ('https://i.ibb.co/ftZy3GR/design7.jpg')
+    imgSrc: require('../design/design7.png')
   },
   {
     id: 45,
-    imgSrc: ('https://i.ibb.co/8PQvFWp/design12.png')
+    imgSrc: require('../design/design12.png')
   },
   {
     id: 46,
-    imgSrc: ('https://i.ibb.co/7rKDccV/design11.png')
+    imgSrc: require('../design/design14.png')
   },
   {
     id: 47,
-    imgSrc: ('https://i.ibb.co/jrp10LM/design14.jpg')
+    imgSrc: require('../design/design15.png')
   },
   {
     id: 48,
-    imgSrc: ('https://i.ibb.co/DLxrDgP/design15.jpg')
+    imgSrc: require('../design/design10.png')
   },
 ]
